479bfaff1e897fc365b37a6037da3830e92d0305
Melvin <melvinatieno@gmail.com>
2018-11-20 13:41:47 +0300

    [update]figaro with rails 5.2

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "66.42.43.168", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "45.32.57.12", user: "deploy", roles: %w{app db web}, my_property: :my_value
 
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
--- lib/capistrano/hooks.rb
+++ lib/capistrano/hooks.rb
@@ -2,14 +2,15 @@
 before "puma:nginx_config", 'check:ssl'
 
 before 'puma:config', 'puma:check_cpu_cores'
+before 'puma:config', 'figaro:setup'
 before 'deploy:updated', 'puma:config'
 before 'deploy:starting', 'check:revision'
 before 'deploy:starting', 'check:first_deploy'
 after 'deploy:publishing', 'deploy:restart'
 after "deploy:publishing", "sidekiq:restart"  
 # after 'deploy:migrate', 'deploy:seed'
-after "deploy:symlink:release", "figaro:setup"
 after "figaro:setup", "figaro:symlink"
+after "deploy:symlink:release", "figaro:setup"
 
 # cap [ENV] deploy:assets_compile
 after 'deploy:assets_compile', 'deploy:restart'
